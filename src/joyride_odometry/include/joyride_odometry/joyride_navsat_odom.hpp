4907cbab876b4aa850c89281c0286c47d95e8303
Max DeSantis <max.desantis@okstate.edu>
2023-03-26 01:38:22 -0500

    NavSat odom working... I think

---
+++ src/joyride_odometry/include/joyride_odometry/joyride_navsat_odom.hpp
@@ -0,0 +1,77 @@
+
+#ifndef JOYRIDE_ODOMETRY__NAVSAT_ODOM_HPP_
+#define JOYRIDE_ODOMETRY__NAVSAT_ODOM_HPP_
+
+#include <memory>
+#include <string>
+
+#include "GeographicLib/LocalCartesian.hpp"
+#include "Eigen/Dense"
+
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "sensor_msgs/msg/nav_sat_fix.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+
+namespace joyride_odometry
+{
+class NavSatOdom : public rclcpp::Node
+{
+public:
+  /**
+   * @brief Constructor
+   */
+  explicit NavSatOdom(const rclcpp::NodeOptions&);
+
+  /**
+   * @brief Destructor
+   */
+  ~NavSatOdom();
+
+private:
+  void gpsFixCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
+  void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg);
+
+  geometry_msgs::msg::TransformStamped buildOdomTF(double x, double y, double z,
+                                                   geometry_msgs::msg::Quaternion orientation, std::string frame_id,
+                                                   std::string child_frame_id);
+
+  nav_msgs::msg::Odometry buildOdomMsg(double x, double y, double z, geometry_msgs::msg::Quaternion orientation,
+                                       std::string frame_id, std::string child_frame_id);
+
+  // Subscribers
+  rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr navSatSub_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imuSub_;
+
+  // Publishers
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odomPub_;
+
+  // Parameters
+  std::string base_link_frame_id_;
+  std::string world_frame_id_;
+  std::string navsat_sub_topic_;
+  std::string imu_sub_topic_;
+  std::string odom_pub_topic_;
+
+  bool broadcast_cartesian_tf_;
+  bool broadcast_odom_;
+
+  // State
+  sensor_msgs::msg::NavSatFix initialLLA_fix_ = sensor_msgs::msg::NavSatFix();
+  double previousYawAngleRadians_;
+
+  // TF2
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::unique_ptr<tf2_ros::TransformListener> tf_listener_;
+  tf2_ros::TransformBroadcaster odom_broadcaster_;
+
+  // LTM
+  GeographicLib::LocalCartesian local_cartesian_plane_;
+};
+
+}  // namespace joyride_odometry
+
+#endif  // JOYRIDE_ODOMETRY__NAVSAT_ODOM_HPP_
