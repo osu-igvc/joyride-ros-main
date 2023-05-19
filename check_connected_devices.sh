1047db2f040f9c5587196ed2d0ac56a0bc4ab582
Max DeSantis <max.desantis@okstate.edu>
2023-05-19 11:58:29 -0500

    Utility script

--- check_connected_devices.sh
+++ check_connected_devices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-interfaces=("nicEthernet1" "nicEthernet2" "nicEthernet3" "nicEthernet4")
+interfaces=("nicEthernet1" "nicEthernet2" "nicEthernet3" "nicEthernet4" "nicEthernet5" "nicEthernet6" "nicEthernet7" "nicEthernet8")
 
 for nic in ${interfaces[@]}; do
     echo "Checking interface: $nic" $'\n'
