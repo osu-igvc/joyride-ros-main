3752c224918708b4cda5e2db3ca1615d2c1f704e
Joyride-OBC <josephine.wade@okstate.edu>
2023-10-17 14:54:21 -0500

    Attempting to remove some file types listed as LFS for git

--- src/joyride_perception/joyride_perception/uv_to_pointcloud.py
+++ src/joyride_perception/joyride_perception/uv_to_pointcloud.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd70bc193d1395f80d992e70884cbd12afe6556601fdaf0cf39dbd32877d682e
-size 6206
+oid sha256:9e44f57f62bb0b599f2f09e8dd18b43b25c39a82e4e8f6d5c76e6e91c789fa3e
+size 6199
