eca9ac6a55af93647e53ea591384c4d3c7ab8f63
Josephine Wade <josephine.wade@okstate.edu>
2023-11-13 16:47:06 -0600

    mapping - random points added donno why

--- src/joyride_bringup/launch/joyride_sensors.launch.py
+++ src/joyride_bringup/launch/joyride_sensors.launch.py
@@ -26,11 +26,11 @@ def generate_launch_description():
     return LaunchDescription([
 
         # Cameras
-        # IncludeLaunchDescription(
-        #    PythonLaunchDescriptionSource([os.path.join(
-        #        get_package_share_directory('blackfly_camera_driver'), 'launch'),
-        #        '/bfly_center_lifecycle.launch.py'])
-        # ),
+        IncludeLaunchDescription(
+           PythonLaunchDescriptionSource([os.path.join(
+               get_package_share_directory('blackfly_camera_driver'), 'launch'),
+               '/bfly_center_lifecycle.launch.py'])
+        ),
 
         # Vectornav INS
         IncludeLaunchDescription(
