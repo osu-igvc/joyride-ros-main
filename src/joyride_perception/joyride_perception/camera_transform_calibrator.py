96896955fc066b8d3f1d890da109204b51276b7b
Kagami-Virus <kv.kagamivirus@gmail.com>
2023-12-11 19:42:49 -0600

    Added comments for definitions, args and returns for camera_transform_calibrator.py

--- src/joyride_perception/joyride_perception/camera_transform_calibrator.py
+++ src/joyride_perception/joyride_perception/camera_transform_calibrator.py
@@ -1,5 +1,12 @@
 # Camera Transform Calibrator
+"""
+Camera Transform Calibrator
 
+This module contains the OpticalTransformCalibrator class, which is a ROS node that calibrates the transformation
+between camera image points and world coordinates. It uses OpenCV for image processing and handles user input for point
+correlation and calibration.
+
+"""
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -28,56 +35,191 @@ def nothing(x):
     pass
 
 class PointXYZ():
+    """
+    Class representing a point in 3D space with X, Y, and Z coordinates.
+
+    Attributes:
+        x (float): X-coordinate.
+        y (float): Y-coordinate.
+        z (float): Z-coordinate.
+
+    """
     def __init__(self, x, y, z):
+        """
+        Initializes a PointXYZ object with given X, Y, and Z coordinates.
+
+        Parameters:
+            x (float): X-coordinate.
+            y (float): Y-coordinate.
+            z (float): Z-coordinate.
+        """
         self.x = x
         self.y = y
         self.z = z
     
     def getString(self):
+        """
+        Returns a formatted string representation of the PointXYZ object.
+
+        Returns:
+            str: Formatted string representation of the point (e.g., 'xyz(1.0,2.0,3.0)').
+        """
         return 'xyz({},{},{})'.format(self.x, self.y, self.z)
 
     def __str__(self):
+        """
+        Returns a string representation of the PointXYZ object.
+
+        Returns:
+            str: String representation of the point.
+        """
         return self.getString()
+    
     def __repr__(self):
+        """
+        Returns a string representation of the PointXYZ object.
+
+        Returns:
+            str: String representation of the point.
+        """
         return self.getString()
 
 class PointUV():
+    """
+    Class representing a point in 2D space with U and V coordinates.
+
+    Attributes:
+        u (float): U-coordinate.
+        v (float): V-coordinate.
+
+    """
     def __init__(self, u, v):
+        """
+        Initializes a PointUV object with given U and V coordinates.
+
+        Parameters:
+            u (float): U-coordinate.
+            v (float): V-coordinate.
+        """
         self.u = u
         self.v = v
 
     def getString(self):
+        """
+        Returns a formatted string representation of the PointUV object.
+
+        Returns:
+            str: Formatted string representation of the point (e.g., 'uv(1.0,2.0)').
+        """
         return 'uv({},{})'.format(self.u, self.v)
     
     def __str__(self):
+        """
+        Returns a string representation of the PointUV object.
+
+        Returns:
+            str: String representation of the point.
+        """
         return self.getString()
+    
     def __repr__(self):
+        """
+        Returns a string representation of the PointUV object.
+
+        Returns:
+            str: String representation of the point.
+        """
         return self.getString()
 
 class CorrelatedPoint():
+    """
+    Class representing a correlated point with optical and world coordinates.
+
+    Attributes:
+        optical (PointUV): Optical coordinates.
+        world (PointXYZ): World coordinates.
+
+    """
     def __init__(self, opticalPoint:PointUV, worldPoint:PointXYZ):
+        """
+        Initializes a CorrelatedPoint object with given optical and world coordinates.
+
+        Parameters:
+            opticalPoint (PointUV): Optical coordinates.
+            worldPoint (PointXYZ): World coordinates.
+        """
         self.optical = opticalPoint
         self.world = worldPoint
 
     def getString(self):
+        """
+        Returns a formatted string representation of the CorrelatedPoint object.
+
+        Returns:
+            str: Formatted string representation of the correlated point (e.g., 'P: uv(1.0,2.0) | xyz(3.0,4.0,5.0)').
+        """
         return 'P: {} | {}'.format(self.optical.getString(), self.world.getString())
+    
     def __str__(self):
+        """
+        Returns a string representation of the CorrelatedPoint object.
+
+        Returns:
+            str: String representation of the correlated point.
+        """
         return self.getString()
+    
     def __repr__(self):
+        """
+        Returns a string representation of the CorrelatedPoint object.
+
+        Returns:
+            str: String representation of the correlated point.
+        """
         return self.getString()
 
 def xDistanceFunction(optical_v, a, b, c, d):
+    """
+    Calculate the X-distance based on the optical V-coordinate using a specified function.
+
+    Parameters:
+        optical_v (float): Optical V-coordinate.
+        a, b, c, d (float): Coefficients for the function.
+
+    Returns:
+        float: X-distance calculated using the function.
+    """
     return a * np.exp(b*optical_v) + c*np.exp(d*optical_v)
 
 def yDistanceFunction(OPTICAL_UV, a, b, c, d, e, f, g):
+    """
+    Calculate the Y-distance based on the optical U and V coordinates using a specified function.
+
+    Parameters:
+        OPTICAL_UV (tuple): Tuple containing optical U and V coordinates.
+        a, b, c, d, e, f, g (float): Coefficients for the function.
+
+    Returns:
+        float: Y-distance calculated using the function.
+    """
     u,v = OPTICAL_UV
     return a + b*u + c*v + d*u*v + e*np.power(v, 2) + f*u*np.power(v, 2) + g*np.power(v,3)
 
 class OpticalTransformCalibrator(Node):
-
-    
     def __init__(self):
         super().__init__('optical_transform_calibrator')
+        """
+        Initialize the OpticalTransformCalibrator Node.
+
+        This method sets up parameters, calibration offsets, grid properties, and initializes variables.
+        It also subscribes to the image topic and sets up a mouse callback.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.world_data_file = self.declare_parameter('data', 'test_xyz_data.csv').get_parameter_value().string_value
         self.camera_frame = self.declare_parameter("camera_frame","").get_parameter_value().string_value
 
@@ -125,6 +267,18 @@ class OpticalTransformCalibrator(Node):
         
 
     def handleNewImage(self, msg:Image):
+        """
+        Callback function to handle new image messages.
+
+        This method processes the incoming image, detects yellow points, and updates the displayed image.
+        If paused, it waits for a spacebar press to perform additional actions.
+
+        Parameters:
+            msg (Image): The incoming image message.
+
+        Returns:
+            None
+        """
         #np_arr = np.fromstring(msg.data, np.uint8)
         #image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         
@@ -154,6 +308,21 @@ class OpticalTransformCalibrator(Node):
 
     # ----------- GUI ----------- #
     def mouseClick(self, event, x, y, flags, params):
+        """
+        Callback function for mouse clicks on the image window.
+
+        This method handles left and right mouse clicks for point removal and pausing the calibration process.
+
+        Parameters:
+            event: The type of mouse event.
+            x (int): The x-coordinate of the mouse click.
+            y (int): The y-coordinate of the mouse click.
+            flags: Additional flags for the mouse event.
+            params: Additional parameters for the callback.
+
+        Returns:
+            None
+        """
         if event == cv2.EVENT_LBUTTONDOWN:
             print(x, ' ', y)
 
@@ -177,6 +346,19 @@ class OpticalTransformCalibrator(Node):
         pass
 
     def clickToPointAfterCalibrated(self, u, v):
+        """
+        Convert image coordinates to world coordinates after calibration.
+
+        This method takes image coordinates (u, v) and uses the calibration parameters to compute the corresponding
+        world coordinates (x, y).
+
+        Parameters:
+            u (int): The image x-coordinate.
+            v (int): The image y-coordinate.
+
+        Returns:
+            Tuple[float, float]: The corresponding world coordinates (x, y).
+        """
         uv_vec = np.array([1, u, u**2, u**3, v, v**2, v**3, u*v, u*v**2, u**2 * v])
 
         x_val = np.dot(uv_vec, self.X_params)
@@ -192,7 +374,17 @@ class OpticalTransformCalibrator(Node):
     
 
     def curvefitPoints(self, correlatedPoints:List[CorrelatedPoint]):
+        """
+        Perform curve fitting on correlated points to obtain calibration parameters.
 
+        This method takes a list of correlated points and performs curve fitting separately for X and Y coordinates.
+
+        Parameters:
+            correlatedPoints (List[CorrelatedPoint]): List of correlated points.
+
+        Returns:
+            None
+        """
         U = np.array([p.optical.u for p in correlatedPoints])
         V = np.array([p.optical.v for p in correlatedPoints])
         X = np.array([p.world.x for p in correlatedPoints])
@@ -212,7 +404,17 @@ class OpticalTransformCalibrator(Node):
         np.savetxt(f"CurveFitParams_{self.camera_frame}.csv",np.array([res_x.x,res_y.x]).T,delimiter=",",header="X_params, Y_params")
 
     def correlatePoints(self):
+        """
+        Correlate grid points and optical points.
 
+        This method correlates the detected optical points with the predefined grid points.
+
+        Parameters:
+            None
+
+        Returns:
+            List[CorrelatedPoint]: List of correlated points.
+        """
         if len(self.point_list) != len(self.WORLD_POINTS):
             print('Unable to correlate. Grid points: {}, Optical points: {}'.format(len(self.WORLD_POINTS), len(self.point_list)))
         else:
@@ -253,6 +455,18 @@ class OpticalTransformCalibrator(Node):
 
 
     def removeNearbyPoint(self, click_u, click_v):
+        """
+        Remove points near the clicked point within a specified radius.
+
+        This method removes points from the detected optical points list that are close to the clicked point.
+
+        Parameters:
+            click_u (int): The clicked point's x-coordinate.
+            click_v (int): The clicked point's y-coordinate.
+
+        Returns:
+            None
+        """
         if self.point_list is None:
             return
 
@@ -267,6 +481,17 @@ class OpticalTransformCalibrator(Node):
         self.image_marked = self.drawYellowPoints(self.image_unmarked, self.point_list)
     
     def maskYellow(self, frame):
+        """
+        Apply a yellow color mask to the input frame.
+
+        This method converts the input frame to the HSV color space and applies a yellow color mask.
+
+        Parameters:
+            frame: The input frame.
+
+        Returns:
+            np.ndarray: The masked image.
+        """
         self.updateSliderValues()
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         lower_yellow = np.array([self.lowH, self.lowS, self.lowV])
@@ -275,6 +500,17 @@ class OpticalTransformCalibrator(Node):
         return yellow_mask
 
     def detectYellowPoints(self, masked_image):
+        """
+        Detect yellow points in a masked image.
+
+        This method detects yellow points in the provided masked image using contour analysis.
+
+        Parameters:
+            masked_image: The input masked image.
+
+        Returns:
+            List[PointUV]: List of detected yellow points.
+        """
         centers = []
         contours, _ = cv2.findContours(masked_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -294,6 +530,18 @@ class OpticalTransformCalibrator(Node):
         return centers
 
     def drawYellowPoints(self, image, points):
+        """
+        Draw yellow points on an image.
+
+        This method draws yellow points on the input image and displays additional information.
+
+        Parameters:
+            image: The input image.
+            points (List[PointUV]): List of yellow points.
+
+        Returns:
+            np.ndarray: The image with drawn yellow points.
+        """
         new_image = image.copy()
         for p in points:
             cv2.circle(new_image, (p.u, p.v), radius=5, color=(0, 255, 0), thickness = -1)
@@ -309,6 +557,17 @@ class OpticalTransformCalibrator(Node):
         # angle = np.tan(42.0/97.0)
 
     def createSlider(self):
+        """
+        Create a window with sliders for adjusting color filtering parameters.
+
+        This method creates a window with sliders for adjusting color filtering parameters such as hue, saturation, and value.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         cv2.namedWindow(self.sliderWindowName)
         cv2.createTrackbar('lowH', self.sliderWindowName, 41, 255, nothing)
         cv2.createTrackbar('lowS', self.sliderWindowName, 38, 255, nothing)
@@ -319,6 +578,17 @@ class OpticalTransformCalibrator(Node):
         cv2.createTrackbar('minArea', self.sliderWindowName, 0, 150, nothing)
 
     def updateSliderValues(self):
+        """
+        Update color filtering parameters based on slider values.
+
+        This method updates the color filtering parameters based on the current values of the sliders.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.lowH = cv2.getTrackbarPos('lowH', self.sliderWindowName)
         self.lowS = cv2.getTrackbarPos('lowS', self.sliderWindowName)
         self.lowV = cv2.getTrackbarPos('lowV', self.sliderWindowName)
@@ -328,6 +598,12 @@ class OpticalTransformCalibrator(Node):
         self.minArea = cv2.getTrackbarPos('minArea', self.sliderWindowName)
 
 def main(args=None):
+    """
+    Main function to initialize and run the Optical Transform Calibrator node.
+
+    This function initializes the ROS Python client library, creates an instance of the OpticalTransformCalibrator node,
+    creates a slider window, and spins the node to handle callbacks.
+    """
     rclpy.init(args=args)
     calib = OpticalTransformCalibrator()
     calib.createSlider()
