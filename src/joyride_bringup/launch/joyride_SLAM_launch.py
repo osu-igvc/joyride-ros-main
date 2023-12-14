a9e45d6b05f8dd8afe2534c831dcde1226866ba2
josephine wade <josephine.wade@okstate.edu>
2023-12-14 16:57:34 -0600

    joyride_bringup config file fixes - WIP

--- src/joyride_bringup/launch/joyride_SLAM_launch.py
+++ src/joyride_bringup/launch/joyride_SLAM_launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
         default_value=os.path.join(get_package_share_directory("joyride_bringup"),
-                                   'config', 'mapper_params_online_async.yaml'),
+                                   'config', 'SLAM_config.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
     start_async_slam_toolbox_node = Node(
